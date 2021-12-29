cacfafc3b757688d3cbe1f7aaa41d642a761f304
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2021-12-29 09:29:26 +0200

    github repo dto

---
+++ lib/github/core/infrastructure/dtos/github_repo/github_repo_dto.dart
@@ -0,0 +1,54 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/core/domain/entities/github_repo/github_repo_entity.dart';
+import 'package:repo_viewer/github/core/domain/entities/repo_owner/repo_owner_entity.dart';
+
+import '../repo_owner/repo_owner_dto.dart';
+
+part 'github_repo_dto.freezed.dart';
+part 'github_repo_dto.g.dart';
+
+///
+/// this function is for handling a field in the json response of the server that 
+/// came back as null
+///
+String _descriptionFromJson(Object? json) {
+    return (json as String?) ?? '';
+}
+
+@freezed
+class GithubRepoDTO with _$GithubRepoDTO {
+    const GithubRepoDTO._();
+
+    const factory GithubRepoDTO({
+        required RepoOwnerDTO owner,
+        required String name,
+        ///
+        /// - everything i put in an annotation constructor needs to be constant
+        /// 
+        /// - the description field can be null in the json response so this function
+        ///   is for returning an empty string in case this field is null
+        ///
+        @JsonKey(fromJson: _descriptionFromJson) required String description,
+        @JsonKey(name: 'stargazers_count') required int stargazersCount,
+    }) = _GithubRepoDTO;
+
+    factory GithubRepoDTO.fromJson(Map<String, dynamic> json) => _$GithubRepoDTOFromJson(json);
+
+    factory GithubRepoDTO.fromDomain(GithubRepoEntity githubRepoEntity) {
+        return GithubRepoDTO(
+            owner: RepoOwnerDTO.fromDomain(githubRepoEntity.owner), 
+            name: githubRepoEntity.name, 
+            description: githubRepoEntity.description, 
+            stargazersCount: githubRepoEntity.stargazersCount
+        );
+    }
+
+    GithubRepoEntity toDomain() {
+        return GithubRepoEntity(
+            owner: owner.toDomain(), 
+            name: name, 
+            description: description, 
+            stargazersCount: stargazersCount
+        );
+    }
+}
